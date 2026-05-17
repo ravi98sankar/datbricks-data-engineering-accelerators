db67cd7e4670886c8d4463c7a3bad33bc019cc64
ravi98sankar <meet.ravisankar@gmail.com>
2026-05-17 02:55:03 +0000

    commit

---
+++ test/New File 2026-05-16 22_53_58.py
@@ -0,0 +1,7 @@
+def are_anagrams(str1, str2):
+    return sorted(str1.replace(" ", "").lower()) == sorted(str2.replace(" ", "").lower())
+
+# Example usage:
+word1 = "listen"
+word2 = "silent"
+result = are_anagrams(word1, word2)
